328b7646e9d098ad4c2b98725ba6cae0297346ff
Kerim <kutukmu@gmail.com>
2020-07-09 07:40:08 -0500

    update

--- src/Components/Seo.js
+++ src/Components/Seo.js
@@ -31,7 +31,7 @@ const SEO = ({ title, desc }) => {
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:creator" content={author} />
         <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
+        <meta name="twitter:description" content={siteDesc} />
         <meta name="twitter:image" content={`${siteUrl}${image}`} />
     </Helmet>
 
